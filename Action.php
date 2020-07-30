9f5dc62acac3f8203cda5e6b14a1594d74cde402
gaobinzhan <gaobinzhan@gmail.com>
2020-07-30 14:44:11 +0800

    add: OfficialAccountService

---
+++ Action.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @author gaobinzhan <gaobinzhan@gmail.com>
+ * @link https://blog.gaobinzhan.com
+ */
+
+class CommentPush_Action extends Typecho_Widget implements Widget_Interface_Do
+{
+    public function action()
+    {
+        // TODO: Implement action() method.
+    }
+
+    public static function officialAccount()
+    {
+        $options = Helper::options();
+
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+
+        $token = $options->plugin('CommentPush')->officialAccountToken;
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode($tmpArr);
+        $tmpStr = sha1($tmpStr);
+
+        if ($tmpStr != $signature) {
+            return false;
+        }
+        return true;
+    }
+}
